1c9e8e2bf926956b0465106f1f1262a7345dbafc
Orpheus LAU <133088502+orpheuslau@users.noreply.github.com>
2024-01-19 17:10:23 +0800

    20240119

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 /*!
-* Start Bootstrap - Freelancer v7.0.7 (https://startbootstrap.com/theme/freelancer)
+* Start Bootstrap - Creative v7.0.7 (https://startbootstrap.com/theme/creative)
 * Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
 */
 //
 // Scripts
@@ -48,7 +48,13 @@ window.addEventListener('DOMContentLoaded', event => {
             if (window.getComputedStyle(navbarToggler).display !== 'none') {
                 navbarToggler.click();
             }
-        });
+        }
+        );
     });
+
+    // Activate SimpleLightbox plugin for portfolio items
+   // new SimpleLightbox({
+     //   elements: '#portfolio a.portfolio-box'
+    //});
 
 });
